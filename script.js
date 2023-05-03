17a4deec17eaf154021ee95f6b903a5802cacd74
Xdsdiaz180X <davidssondiaz@gmail.com>
2023-05-03 15:28:21 -0500

    creacion de la logica de encriptar el texto

---
+++ script.js
@@ -0,0 +1,29 @@
+var textoEncriptacion = [];
+
+function encriptar() {
+  let texto = document.getElementById("espacioTexto").value;
+  const reglas = {
+    e: "enter",
+    i: "imes",
+    a: "ai",
+    o: "ober",
+    u: "ufat",
+  };
+
+  var textoEncriptado = "";
+  for (let i = 0; i < texto.length; i++) {
+    let letra = texto[i];
+    let letraEncriptada = reglas[letra] || letra;
+    textoEncriptado += letraEncriptada;
+  }
+  textoEncriptacion.push(textoEncriptado);
+  console.log(textoEncriptacion);
+  document.querySelector("#formularioTexto").reset();
+
+  mostrarTextoEncriptado();
+}
+
+function mostrarTextoEncriptado() {
+  const textoFinal = textoEncriptacion[textoEncriptacion.length - 1];
+  document.getElementById("cajaTexto").innerHTML = textoFinal;
+}
