ddc6fe7dbda2495b0793d47052ed0930250730c6
Xdsdiaz180X <davidssondiaz@gmail.com>
2023-05-03 16:52:09 -0500

    cambio en el boton de copiar

--- script.js
+++ script.js
@@ -26,26 +26,6 @@ function encriptar() {
 function mostrarTextoEncriptado() {
   const textoFinal = textoEncriptacion[textoEncriptacion.length - 1];
   document.getElementById("cajaTexto").innerHTML = textoFinal;
-  const espacioBoton = document.querySelector("#cajaTexto");
-  let botonCopiar = document.createElement("button");
-  const textoBoton = document.createTextNode("Copiar");
-  botonCopiar.appendChild(textoBoton);
-  botonCopiar.setAttribute("id", "botonCopiar");
-  espacioBoton.appendChild(botonCopiar);
-  const boton = document.querySelector("#botonCopiar");
-
-  botonCopiar.addEventListener("click", function () {
-    const texto = document.getElementById("cajaTexto").textContent;
-
-    navigator.clipboard
-      .writeText(texto)
-      .then(() => {
-        alert("¡Texto copiado al portapapeles!");
-      })
-      .catch((error) => {
-        alert(`Error al copiar el texto: ${error}`);
-      });
-  });
 }
 
 
