63a5998388ace73e76091f12116e4837c74c8bc6
Xdsdiaz180X <davidssondiaz@gmail.com>
2023-05-06 17:26:25 -0500

    ultimos retoques css y javascript

--- script.js
+++ script.js
@@ -26,11 +26,38 @@ function encriptar() {
 function mostrarTextoEncriptado() {
   const textoFinal = textoEncriptacion[textoEncriptacion.length - 1];
   const cajaTexto = document.getElementById("cajaTexto");
-  cajaTexto.innerHTML = textoFinal;
+  cajaTexto.style.fontFamily = "Roboto Mono";
+  cajaTexto.innerHTML = "";
+
+  const parrafo = document.createElement("p");
+  parrafo.innerHTML = textoFinal;
+  cajaTexto.appendChild(parrafo);
+
+  parrafo.style.fontSize = "20px";
+  parrafo.style.color = "darkorange";
+  parrafo.style.padding = "20px";
+  parrafo.style.marginTop = "50px";
 
   const botonCopiar = document.createElement("button");
   botonCopiar.innerHTML = "Copiar";
   cajaTexto.appendChild(botonCopiar);
+
+  botonCopiar.style.backgroundColor = "rgb(203, 202, 202)";
+  botonCopiar.style.fontFamily = "Roboto Mono";
+  (botonCopiar.style.padding = "20px"), "0";
+  botonCopiar.style.width = "80%";
+  botonCopiar.style.fontSize = "18px";
+  botonCopiar.style.fontWeight = "bold";
+  botonCopiar.style.border = "none";
+  botonCopiar.style.borderRadius = "20px";
+  (botonCopiar.style.transition = "1s"), "all";
+  botonCopiar.style.cursor = "pointer";
+  botonCopiar.style.marginTop = "600px";
+
+  botonCopiar.addEventListener("mouseover", function () {
+    this.style.backgroundColor = "darkgrey";
+    this.style.transform = "scale(1.1)";
+  });
 
   botonCopiar.addEventListener("click", (e) => {
     const elementoTemporal = document.createElement("textarea");
@@ -71,21 +98,44 @@ function desencriptar() {
     palabraDesencriptada += letraDesencriptada;
   }
 
-  textoDesencriptacion.push(palabraDesencriptada)
+  textoDesencriptacion.push(palabraDesencriptada);
   console.log(palabraDesencriptada);
   document.querySelector("#formularioTexto").reset();
-  
-  mostrarTextoDesencriptado()
+
+  mostrarTextoDesencriptado();
 }
 
 function mostrarTextoDesencriptado() {
-  const textoFinalDesencriptado = textoDesencriptacion[textoDesencriptacion.length - 1];
+  const textoFinalDesencriptado =
+    textoDesencriptacion[textoDesencriptacion.length - 1];
   const cajaTextoDesencriptada = document.getElementById("cajaTexto");
-  cajaTextoDesencriptada.innerHTML = textoFinalDesencriptado;
+  cajaTextoDesencriptada.style.fontFamily = "Roboto Mono";
+  cajaTextoDesencriptada.innerHTML = "";
+
+  const parrafoDes = document.createElement("p");
+  parrafoDes.innerHTML = textoFinalDesencriptado;
+  cajaTexto.appendChild(parrafoDes);
+
+  parrafoDes.style.fontSize = "20px";
+  parrafoDes.style.color = "darkorange";
+  parrafoDes.style.padding = "20px";
+  parrafoDes.style.marginTop = "50px";
 
   const botonCopiar2 = document.createElement("button");
   botonCopiar2.innerHTML = "Copiar";
   cajaTexto.appendChild(botonCopiar2);
+
+  botonCopiar2.style.backgroundColor = "rgb(203, 202, 202)";
+  botonCopiar2.style.fontFamily = "Roboto Mono";
+  (botonCopiar2.style.padding = "20px"), "0";
+  botonCopiar2.style.width = "80%";
+  botonCopiar2.style.fontSize = "18px";
+  botonCopiar2.style.fontWeight = "bold";
+  botonCopiar2.style.border = "none";
+  botonCopiar2.style.borderRadius = "20px";
+  (botonCopiar2.style.transition = "1s"), "all";
+  botonCopiar2.style.cursor = "pointer";
+  botonCopiar2.style.marginTop = "600px";
 
   botonCopiar2.addEventListener("click", (i) => {
     const elementoTemporal = document.createElement("textarea");
